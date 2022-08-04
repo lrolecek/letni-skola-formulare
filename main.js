5c979f3d9a92b5a7f1c58ee83af91879f3e2f723
Luděk Roleček <ludek@rolecek.cz>
2022-08-04 11:17:19 +0200

    Řešení

--- main.js
+++ main.js
@@ -1,2 +1,43 @@
 console.log('JavaScript funguje');
 
+
+
+let formular = document.querySelector('#formular')
+formular.addEventListener('submit', zpracujFormular)
+
+
+function zpracujFormular(event) {
+	event.preventDefault()
+
+	console.log('zpracovavam')
+
+	let jmeno = document.querySelector('#jmeno').value
+	let prijmeni = document.querySelector('#prijmeni').value
+	console.log(jmeno + ' ' + prijmeni)
+
+	let datum = document.querySelector('#datum').value
+	console.log(datum)
+
+	let barva = document.querySelector('#barva').value
+	console.log(barva)
+
+	let mesto = document.querySelector('#mesto').value
+	console.log(mesto)
+
+	let souhlas = document.querySelector('#souhlas').checked
+	if (souhlas == true) {
+		console.log('Zákoš souhlasí, pošleme mu spam')
+	} else {
+		console.log('Tenhle nic nechce posílat :(')
+	}
+
+}
+
+
+let barvicka = document.querySelector('#barva')
+barvicka.addEventListener('change', zmenBarvu)
+
+function zmenBarvu() {
+	let barva = document.querySelector('#barva').value
+	document.querySelector('body').style.backgroundColor = barva;
+}
